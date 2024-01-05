70072b57e2d523cb7fe8fa9ae84e2622a05583f5
AKD <contact.mousom@gmail.com>
2024-01-05 20:14:17 +0600

    Dark theme is created and apply for the navigation bar

--- src/Components/Shared/NavigationBar/Navigation.jsx
+++ src/Components/Shared/NavigationBar/Navigation.jsx
@@ -2,32 +2,45 @@ import {
     AppBar,
     Box,
     Container,
-    Link, Stack, Toolbar,
-    Typography
+    Link, Stack, ThemeProvider, Toolbar,
+    Typography,
+    createTheme
 } from "@mui/material";
+
+
+const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        main: '#1976d2',
+      },
+    },
+  });
 
 const Navigation = () => {
     return (
         <>
-            <AppBar>
-                <Container>
-                    <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <ThemeProvider theme={darkTheme}>
+                <AppBar>
+                    <Container>
+                        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
 
-                        <Toolbar>
-                            <Typography >TODO WITH MATERIAL</Typography>
-                        </Toolbar>
+                            <Toolbar>
+                                <Typography >TODO WITH MATERIAL</Typography>
+                            </Toolbar>
 
 
 
-                        <Stack direction={"row"} spacing={5}>
-                            <Link href="#" underline="none" color="inherit">Home</Link>
-                            <Link href="#" underline="none" color="inherit">About</Link>
-                            <Link href="#" underline="none" color="inherit">Contact</Link>
-                            <Link href="#" underline="none" color="inherit">Login</Link>
-                        </Stack>
-                    </Box>
-                </Container>
-            </AppBar>
+                            <Stack direction={"row"} spacing={5}>
+                                <Link href="#" underline="none" color="inherit">Home</Link>
+                                <Link href="#" underline="none" color="inherit">About</Link>
+                                <Link href="#" underline="none" color="inherit">Contact</Link>
+                                <Link href="#" underline="none" color="inherit">Login</Link>
+                            </Stack>
+                        </Box>
+                    </Container>
+                </AppBar>
+            </ThemeProvider>
         </>
     );
 };
