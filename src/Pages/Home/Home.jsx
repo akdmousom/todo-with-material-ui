5f3448b11a6cfa75986e404f1d57bf019d8670a3
AKD <contact.mousom@gmail.com>
2024-01-09 23:32:14 +0600

    Todo Text Field And Custom Button Is Now Working Well

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,63 @@
+import { Box, Container, TextField, Typography } from "@mui/material";
+import { AddButton } from "../../Components/CustomComponents/CustomButton/CustomButton";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const Home = () => {
+    const [addNote, setAddNote] = useState([])
+
+    const {
+        register,
+        handleSubmit,
+        
+      } = useForm();
+
+      const onSubmit = (data) => {
+
+        const Note = data?.Note;
+
+        const prevNote = [...addNote]
+        const newNote = [...prevNote,Note]
+        setAddNote([newNote])
+       
+      }
+
+      console.log(addNote);
+
+  
+
     return (
-        <div>
-            this the home page
-        </div>
+        <Container>
+            <Box sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
+
+
+                <form onSubmit={handleSubmit(onSubmit)}>
+
+                    <TextField
+                        id="outlined-controlled"
+                        label="Add Note"
+                        name="addNote"
+                        {...register('Note')}
+
+                    />
+
+                    <AddButton type="submit">Add Note</AddButton>
+
+                </form>
+
+
+            </Box>
+
+            <Box sx={{display:'flex', flexFlow:'column'}}>
+                {addNote.map((item,idx) => {
+                    return (
+                    
+                             <><Typography key={idx}>{item}</Typography><br /></>
+                     
+                    )
+                })}
+            </Box>
+        </Container>
     );
 };
 
